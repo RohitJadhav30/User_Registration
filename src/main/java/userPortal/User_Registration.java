f7d326fb951b84da5482c52f3b5645edf590d373
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-07-12 17:35:05 +0530

    UC2 validating last name

--- src/main/java/userPortal/User_Registration.java
+++ src/main/java/userPortal/User_Registration.java
@@ -3,20 +3,23 @@ package userPortal;
 import java.util.Scanner;
 
 public class User_Registration {
-    public static boolean validateFirstName(String firstName) {
-        return firstName.matches("^[A-Z][a-zA-Z]{2,}$");
+    public static boolean validateFirstName(String firstName, String lastname) {
+        return firstName.matches("^[A-Z][a-zA-Z]{2,}$") && lastname.matches("^[A-Z][a-zA-Z]{2,}$");
     }
 
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	System.out.print("Enter the first name: ");
         String firstName = sc.nextLine();
+        
+        System.out.print("Enter the last name: ");
+        String lastName = sc.nextLine();
         sc.close();
         
-        if (validateFirstName(firstName)) {
-            System.out.println(firstName + " is a valid first name.");
+        if (validateFirstName(firstName , lastName)) {
+            System.out.println(firstName + " " + lastName + " is a valid name.");
         } else {
-            System.out.println(firstName + " is not a valid first name.");
+            System.out.println(firstName + " " + lastName + " is not a valid name.");
         }
     }
 }
