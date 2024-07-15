40fa139d9bcb73f69656b3a954a9a887359c689c
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-07-15 14:42:05 +0530

    UC6 validating password rule2

--- src/main/java/userPortal/User_Registration.java
+++ src/main/java/userPortal/User_Registration.java
@@ -28,8 +28,15 @@ public class User_Registration {
     
     //password
     public static boolean validatePassword(String password) {
-    	return password.length() >= 8;
-    	
+    	//rule1
+    	if(password.length() < 8) {
+    		return false;
+    	}
+    	//rule2
+    	if(!password.matches(".*[A-Z].*")) {
+    		return false;
+    	}
+    	return true;
     }
 
     public static void main(String[] args) {
