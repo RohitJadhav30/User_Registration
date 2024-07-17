77b2def52e70b7002a70134321843e80221505ad
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-07-17 12:59:25 +0530

    UC11 using multiple email samples

--- src/main/java/userPortal/User_Registration.java
+++ src/main/java/userPortal/User_Registration.java
@@ -16,7 +16,7 @@ public class User_Registration {
 
     //email
     public static boolean validateEmail(String email) {
-        String emailRegex = "^[a-z]+(\\.[a-z]+)?@[a-z]+\\.[a-z]{2,}(\\.[a-z]{2,})?$";
+        String emailRegex = "^[a-zA-Z0-9.%+-]+\\.[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}(\\.[a-zA-Z]{2,6})?$";
         return email.matches(emailRegex);
     }
     
