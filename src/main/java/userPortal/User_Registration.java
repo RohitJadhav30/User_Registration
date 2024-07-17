d318dd26da935eb6c96f0cf2c7f559a5dc9f3717
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-07-17 17:07:29 +0530

    UC12 adding custom exceptions

--- src/main/java/userPortal/User_Registration.java
+++ src/main/java/userPortal/User_Registration.java
@@ -1,88 +1,46 @@
 package userPortal;
 
-import java.util.Scanner;
+import UserException.InvalidInputException;
 
 public class User_Registration {
 
 	//firstName and LastName
-    public static boolean validateName(String name) {
-        return name.matches("^[A-Z][a-zA-Z]{2,}$");
-    }
-
-    //special characters
-    public static String removeSpecialCharacters(String input) {
-        return input.replaceAll("[^a-zA-Z0-9 ]", "");
+    public static boolean validateName(String name) throws InvalidInputException{
+        if(!name.matches("^[A-Z][a-zA-Z]{2,}$")){
+        	throw new InvalidInputException("Invalid name format: " + name);
+        }
+        return true;
     }
 
     //email
-    public static boolean validateEmail(String email) {
+    public static boolean validateEmail(String email) throws InvalidInputException{
         String emailRegex = "^[a-zA-Z0-9.%+-]+\\.[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}(\\.[a-zA-Z]{2,6})?$";
-        return email.matches(emailRegex);
+        if(!email.matches(emailRegex)) {
+        	throw new InvalidInputException("Invalid email format: " + email);
+        }
+        return true;
     }
     
     //mobile number
-    public static boolean validateMobileNumber(String mobileNumber) {
+    public static boolean validateMobileNumber(String mobileNumber) throws InvalidInputException{
         String mobileRegex = "^(91 )?\\d{10}$";
-        return mobileNumber.matches(mobileRegex);
+        if(!mobileNumber.matches(mobileRegex)) {
+        	throw new InvalidInputException("Invalid mobile input: " + mobileNumber);
+        }
+        return true;
     }
     
-    //password
-    public static boolean validatePassword(String password) {
-
-    	String PassRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.{0,9}[!@#$%^&*()_+=-{};:'<>,./?][^!@#$%^&*()_+=-{};:'<>,./?]{0,7})(?!.*[@.]).{8,}$";
-    	return password.matches(PassRegex);
+    //Password
+    public static boolean validatePassword(String password) throws InvalidInputException {
+        String passRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=\\-{}\\[\\]:;<>,./?])(?!.*[@.])(?=\\S+$).{8,}$";
+        if (!password.matches(passRegex)) {
+            throw new InvalidInputException("Invalid password format: " + password);
+        }
+        return true;
     }
 
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the first name: ");
-        String firstName = sc.nextLine();
-        firstName = removeSpecialCharacters(firstName);
-
-        System.out.print("Enter the last name: ");
-        String lastName = sc.nextLine();
-        lastName = removeSpecialCharacters(lastName);
-
-        System.out.print("Enter the emails (separated by commas): ");
-        String emailsInput = sc.nextLine();
-
-        String[] emails = emailsInput.split(",");
-        boolean allEmailsValid = true;
-        for (String email : emails) {
-            email = email.trim();
-            if (validateEmail(email)) {
-                System.out.println(email + " is a valid email.");
-            } else {
-                System.out.println(email + " is not a valid email.");
-                allEmailsValid = false;
-            }
-        }
         
-        System.out.print("Enter the mobile number: ");
-        String mobileNo = sc.nextLine();
-        
-        System.out.print("Enter the password: ");
-        String password = sc.next();
-
-        sc.close();
-
-        if (validateName(firstName) && validateName(lastName)) {
-            System.out.println(firstName + " " + lastName + " is a valid name.");
-        } else {
-            System.out.println(firstName + " " + lastName + " is not a valid name.");
-        }
-        
-        if (validateMobileNumber(mobileNo)) {
-            System.out.println(mobileNo + " is a valid mobile number.");
-        } else {
-            System.out.println(mobileNo + " is not a valid mobile number.");
-        }
-        
-        if (validatePassword(password)) {
-            System.out.println(password + " is a valid password.");
-        } else {
-            System.out.println(password + " is not a valid password.");
-        }
     }
 }
